e6febec993d462cfd14e92674f2aaa5fcbcb156f
bigfu <bigfu1993@163.com>
2023-01-05 10:14:18 +0800

    data type

---
+++ 01/Plus.java
@@ -0,0 +1,11 @@
+public class Plus{
+    public static void main(String[] args){
+        System.out.println("bigfu"+1);
+    }
+}
+
+class PlusDog{
+    public static void main(String[] args){
+        System.out.println(10+20);
+    }
+}
