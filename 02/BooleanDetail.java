c033e0526999c8109c0634956d09e6abcd1ae802
bigfu <bigfu1993@163.com>
2023-01-09 01:42:38 +0800

    char

---
+++ 02/BooleanDetail.java
@@ -0,0 +1,10 @@
+public class BooleanDetail{
+    public static void main(String[] args){
+        boolean isPass = true;
+        if(isPass){
+            System.out.println("考试通过");
+        }else{
+            System.out.println("考试不通过");
+        }
+    }
+}
