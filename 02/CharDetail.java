5ea2da9e7c1db7aaa2458d3cc043de974a465193
bigfu <bigfu1993@163.com>
2023-01-08 23:36:51 +0800

    char

---
+++ 02/CharDetail.java
@@ -0,0 +1,13 @@
+public class CharDetail{
+    public static void main(String[] args){
+        char c1 = 'a';
+        char c2 = '\t';
+        char c3 = '³Â';
+        char c4 = 97;
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        System.out.println(c4);
+        System.out.println((int)c3);
+    }
+}
