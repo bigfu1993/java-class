5e3938a49147cb247c94264a1c99dd5358e3cb8f
bigfu <bigfu1993@163.com>
2023-02-09 18:21:34 +0800

    for

--- 03/For.java
+++ 03/For.java
@@ -1,16 +1,25 @@
 public class For {
     public static void main(String[] args){
-        //  1-100 9的倍数有几个分别是什么
-        int count = 0;
-        String s = "";
+        // //  1-100 9的倍数有几个分别是什么
+        // int count = 0;
+        // String s = "";
+        // for(int i = 0;i<=100;i++){
+        //     if(i>9&&i%9==0){
+        //         count+=1;
+        //         s=s+i+"、";
+        //     }
+        // }
+        // System.out.println("9的倍数有"+count+"个，分别是："+s);
 
-        for(int i = 0;i<=100;i++){
-            if(i>9&&i%9==0){
-                count+=1;
-                s=s+i+"、";
+        int line = 4;
+        for(int i = 0;i<line;i++){
+            for(int k = 0;k<line-i;k++){
+                System.out.print(" ");
             }
+            for(int j = 0;j<=i;j++){
+                System.out.print("*");
+            } 
+            System.out.println('\r');
         }
-
-        System.out.println("9的倍数有"+count+"个，分别是："+s);
     }
 }
