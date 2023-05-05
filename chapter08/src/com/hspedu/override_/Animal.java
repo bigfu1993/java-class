e2f690b3d064046e51a86be62b0ffcda3d1246b9
bigfu@home-pc <bigfu1993@163.com>
2023-05-05 22:25:56 +0800

    override

---
+++ chapter08/src/com/hspedu/override_/Animal.java
@@ -0,0 +1,7 @@
+package com.hspedu.override_;
+
+public class Animal {
+    public void say(){
+        System.out.println("animal say");
+    }
+}
---
+++ chapter08/src/com/hspedu/override_/Dog.java
@@ -0,0 +1,7 @@
+package com.hspedu.override_;
+
+public class Dog extends Animal {
+    public void say(){
+        System.out.println("dog say");
+    }
+}
