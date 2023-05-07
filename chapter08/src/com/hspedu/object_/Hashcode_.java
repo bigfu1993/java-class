62720be7439a2c3014b07691999990d046057e57
bigfu@home-pc <bigfu1993@163.com>
2023-05-07 21:54:53 +0800

    hashcode

---
+++ chapter08/src/com/hspedu/object_/Hashcode_.java
@@ -0,0 +1,14 @@
+package com.hspedu.object_;
+
+public class Hashcode_ {
+    public static void main(String[] args) {
+        AA aa = new AA();
+        AA aa1 = new AA();
+        AA aa2 = aa;
+        System.out.println(aa.hashCode());
+        System.out.println(aa1.hashCode());
+        System.out.println(aa2.hashCode());
+    }
+}
+
+class AA {}
