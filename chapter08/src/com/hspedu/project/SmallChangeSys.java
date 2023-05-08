308cb25d28c2e95ded1cf768fda5c28acda59c07
bigfu@home-pc <bigfu1993@163.com>
2023-05-08 23:56:00 +0800

    project/零钱通

--- chapter08/src/com/hspedu/project/SmallChangeSys.java
+++ chapter08/src/com/hspedu/project/SmallChangeSys.java
@@ -7,19 +7,21 @@ public class SmallChangeSys {
         boolean loop = true;
         Scanner scanner = new Scanner(System.in);
         String key = "";
+        String details = "";
         do{
             System.out.println("======= 零钱通菜单 =======");
-            System.out.println("\t\t\t1 零钱通明细");
-            System.out.println("\t\t\t2 收益入账");
-            System.out.println("\t\t\t3 消费");
-            System.out.println("\t\t\t4 exit");
-
+            System.out.println("1 零钱通明细");
+            System.out.println("2 收益入账");
+            System.out.println("3 消费");
+            System.out.println("4 exit");
+            System.out.println("------- 明 细 -------");
             System.out.println("请选择(1-4)：");
             key = scanner.next();
 
             switch (key){
                 case "1" :
                     System.out.println("1 零钱通明细");
+
                     break;
                 case "2" :
                     System.out.println("2 收益入账");
