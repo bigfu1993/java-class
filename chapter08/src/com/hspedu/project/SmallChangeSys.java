9484fd3b1839db386ebca36714fb9d12c6093952
bigfu@home-pc <bigfu1993@163.com>
2023-05-09 00:30:03 +0800

    project/零钱通

--- chapter08/src/com/hspedu/project/SmallChangeSys.java
+++ chapter08/src/com/hspedu/project/SmallChangeSys.java
@@ -1,5 +1,7 @@
 package com.hspedu.project;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class SmallChangeSys {
@@ -8,28 +10,38 @@ public class SmallChangeSys {
         Scanner scanner = new Scanner(System.in);
         String key = "";
         String details = "";
-        do{
+        double money = 0;
+        double balance = 0;
+        Date date = null;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+
+        do {
             System.out.println("======= 零钱通菜单 =======");
             System.out.println("1 零钱通明细");
             System.out.println("2 收益入账");
             System.out.println("3 消费");
             System.out.println("4 exit");
-            System.out.println("------- 明 细 -------");
-            System.out.println("请选择(1-4)：");
+            System.out.print("请选择菜单(1-4)：");
             key = scanner.next();
 
-            switch (key){
-                case "1" :
-                    System.out.println("1 零钱通明细");
-
+            switch (key) {
+                case "1":
+                    System.out.println("零钱通明细");
+                    System.out.println("------- 明 细 -------");
+                    System.out.println(details);
                     break;
-                case "2" :
-                    System.out.println("2 收益入账");
+                case "2":
+                    System.out.print("请输入收益入账：");
+                    money = scanner.nextDouble();
+                    System.out.println("入账金额：¥" + money);
+                    balance += money;
+                    date = new Date();
+                    details += "入账\t¥" + money + "\t" + sdf.format(new Date()) + "\t余额" + balance + "\n";
                     break;
-                case "3" :
+                case "3":
                     System.out.println("3 消费");
                     break;
-                case "4" :
+                case "4":
                     System.out.println("4 exit");
                     loop = false;
                     break;
@@ -37,6 +49,6 @@ public class SmallChangeSys {
                     System.out.println("选择有误");
                     break;
             }
-        }while (loop);
+        } while (loop);
     }
 }
